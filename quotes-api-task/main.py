583ee02d9159116abc2721997bba05786e87a7ae
adhamamr <adhamamr002@gmail.com>
2026-05-07 15:32:17 +0300

    add tracking feature create report

--- quotes-api-task/main.py
+++ quotes-api-task/main.py
@@ -1,7 +1,10 @@
+import csv
+import pandas
 import requests
 import random
+import pandas as pd
 from flask import Flask
-
+import datetime
 
 
 def protected(func):
@@ -37,17 +40,43 @@ def protected(func):
 
 app = Flask(__name__)
 
+track_list = []
+
 
 @app.route('/')
 def hello_world():
     return f'Home Page'
 
+def api_calls():
+    total_calls = sum([item['count'] for item in track_list])
+
+    if total_calls > 0 and total_calls % 10 == 0:
+        time_now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"quotes_api_report_{time_now}.csv"
+
+        df = pandas.DataFrame(track_list)
+        df.columns = ['Quote ID', 'Count']
+        df.to_csv(filename, index=False)
+
+
+
+
 @app.route('/quote/random/')
 @protected
 def random_quote(auth_data, quotes_data):
+
     select_dict = random.choice(quotes_data)
     random_quote_id = select_dict['id']
     random_quote = select_dict["quote"]
+
+    found = False
+    for item in track_list:
+        if random_quote_id == item['id']:
+            item['count'] += 1
+            found = True
+            break
+    if not found:
+        track_list.append({'id': random_quote_id, 'count': 1})
 
 
     for author in auth_data:
@@ -57,10 +86,10 @@ def random_quote(auth_data, quotes_data):
         else:
             name = "UNKNOWN NAME"
 
-
+    print(track_list)
     print(random_quote)
+    api_calls()
     return f'<p>Quote Of The Day: {random_quote} BY: {name}</p>'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
